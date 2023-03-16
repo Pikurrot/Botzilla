9fdc865b47b073adb3e65789af2fb14387ce8d80
Eric Lopez <pikurrot@gmail.com>
2023-03-16 22:28:54 +0100

    Add "top_movies" command

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 from keep_alive import keep_alive
+import imdb
 
+ia = imdb.IMDb()
+
+# bot
 load_dotenv()
 
 intents = Intents.default()
@@ -22,8 +26,24 @@ async def m(ctx, *, prompt):
 
   await msg.edit(content=f"You said {prompt}, I say xd")
 
+@bot.command()
+async def top_movies(ctx):
+  msg = await ctx.send("Accessing IMDb API...")
 
-    await msg.edit(content=f"You said {prompt}, I say xd")
+  top250 = ia.get_top250_movies()
+  lst = "\n".join([movie['title'] for movie in top250[:10]])
+  
+  await msg.edit(content=lst)
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:  # ignore messages sent by the bot itself
+        return
+
+    if message.content.lower() == "hello":
+        await message.channel.send("Hello!")
+
+    await bot.process_commands(message)
 
 keep_alive()
 bot.run(os.environ["DISCORD_TOKEN"])

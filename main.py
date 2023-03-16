d96bccf52c487ed3813e2f10b7dc439eff1021ef
Eric Lopez <90217719+Pikurrot@users.noreply.github.com>
2023-03-16 09:05:06 +0000

    Create functional bot

---
+++ main.py
@@ -0,0 +1,26 @@
+from discord import Intents
+from discord.ext import commands
+from dotenv import load_dotenv
+import os
+from keep_alive import keep_alive
+
+load_dotenv()
+
+intents = Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    description="description haha xd",
+    intents=intents,
+)
+
+
+@bot.command()
+async def dream(ctx, *, prompt):
+    msg = await ctx.send(f"“{prompt}”\n> Generating...")
+
+    await msg.edit(content=f"You said {prompt}, I say xd")
+
+keep_alive()
+bot.run(os.environ["DISCORD_TOKEN"])
